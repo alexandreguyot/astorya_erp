c83031b7fc2eb3b2960c5e9fa8006d4afce4c377
Alexandre Guyot <a.pro.guyot@gmail.com>
2025-05-12 21:52:52 +0200

    Mise a jour

--- app/Jobs/GenerateBillPdf.php
+++ app/Jobs/GenerateBillPdf.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 class GenerateBillPdf implements ShouldQueue
 {
     use InteractsWithQueue, Queueable, SerializesModels;
@@ -26,6 +26,8 @@ class GenerateBillPdf implements ShouldQueue
 
     public function handle(): void
     {
+        Log::info('Generating PDF for bill: ' . $this->no_bill);
+
         $bills = Bill::with([
             'contract',
             'contract.type_period',
@@ -47,6 +49,8 @@ class GenerateBillPdf implements ShouldQueue
 
         $path = "private/factures/{$period_bills}/{$filename}";
 
+        Log::info('PDF path: ' . $path);
+
         $contracts = collect();
         foreach ($bills as $bill) {
             $contracts->push($bill->contract);
@@ -57,8 +61,9 @@ class GenerateBillPdf implements ShouldQueue
 
         $contract = $bills->first()->contract;
         $owner = Owner::first();
-        $vatResumes = app()->call('App\Http\Controllers\Admin\BillController@getVatResumesFromContracts', ['contracts' => $contracts, 'date' => $date]);
-        $totals = app()->call('App\Http\Controllers\Admin\BillController@getTotalsFromVatResumes', ['vatResumes' => $vatResumes]);
+        
+        $vatResumes = $this->getVatResumesFromContracts($contracts, $date);
+        $totals = $this->getTotalsFromVatResumes($vatResumes);
 
         $products = collect();
         foreach ($contracts as $contract) {
@@ -85,5 +90,63 @@ class GenerateBillPdf implements ShouldQueue
 
         Storage::put($path, $pdf->output());
 
+    }
+
+    protected function getVatResumesFromContracts($contracts, $date = null)
+    {
+        $vatResumes = [];
+        foreach ($contracts as $contract) {
+            foreach ($contract->contract_product_detail as $detail) {
+                $vat = $detail->type_product->type_vat ?? null;
+                if (!$vat) continue;
+
+                $key = $vat->code_vat;
+
+                $ht = $detail->proratedBase($date);
+                $tva = $detail->proratedWithVat($date) - $ht;
+
+                if (!isset($vatResumes[$key])) {
+                    $vatResumes[$key] = [
+                        'code' => $vat->code_vat,
+                        'account' => $vat->account_vat,
+                        'percent' => $vat->percent,
+                        'amount_ht' => 0,
+                        'amount_tva' => 0,
+                    ];
+                }
+
+                $vatResumes[$key]['amount_ht'] += $ht;
+                $vatResumes[$key]['amount_tva'] += $tva;
+            }
+        }
+
+        return collect($vatResumes)->map(function ($item) {
+            return [
+                'code' => $item['code'],
+                'account' => $item['account'],
+                'percent' => number_format($item['percent'], 2, ',', ' '),
+                'amount_ht' => number_format($item['amount_ht'], 2, ',', ' '),
+                'amount_tva' => number_format($item['amount_tva'], 2, ',', ' '),
+            ];
+        });
+    }
+
+    protected function getTotalsFromVatResumes($vatResumes)
+    {
+        $totalHt = 0;
+        $totalTva = 0;
+
+        foreach ($vatResumes as $item) {
+            $ht = (float) str_replace([' ', ','], ['', '.'], $item['amount_ht']);
+            $tva = (float) str_replace([' ', ','], ['', '.'], $item['amount_tva']);
+            $totalHt += $ht;
+            $totalTva += $tva;
+        }
+
+        return [
+            'total_ht' => number_format($totalHt, 2, ',', ' '),
+            'total_tva' => number_format($totalTva, 2, ',', ' '),
+            'total_ttc' => number_format($totalHt + $totalTva, 2, ',', ' '),
+        ];
     }
 }
